0d7a7fa74afc89559338c1d68fd760ecafa8e4cf
Sudarshan Kankal <sudarshankankal@gmail.com>
2021-07-19 17:48:55 +0530

    test cases for login component

--- src/tests/components/Login.test.js
+++ src/tests/components/Login.test.js
@@ -4,16 +4,59 @@ import { Login } from '../../components/Login';
 
 
 describe('Given Login Component',() => {
+    
+    const wrapper = shallow(<Login/>);
 
     test('Should render properly',async () => {
-        const wrapper = shallow(<Login/>);
         expect(wrapper).toMatchSnapshot();
     })
 
-    test("render email input tag",() => {
-        const wrapper = shallow(<Login/>);
+    test("should render email input",() => {
         const isPresent = wrapper.find('input[name="email"]').exists();
         expect(isPresent).toBe(true);
-
     })
+
+    test("should render password input",() => {
+        const isPresent = wrapper.find('input[name="password"]').exists();
+        expect(isPresent).toBe(true);
+    })
+
+    test("should render submit button",() => {
+        const isPresent = wrapper.find('button[type="submit"]').exists();
+        expect(isPresent).toBe(true);
+    })
+
+    test("Default value for both the field should be empty",() => {
+        const email = wrapper.find('input[name="email"]').prop('value');
+        const password = wrapper.find('input[name="password"]').prop('value');
+        // console.log(`Email: ${email} Password: ${password}`);
+        expect(email).toBe('');
+        expect(password).toBe('');
+    })
+
+    test("on enter email value of the state should update",() => {
+        // const wrapper = shallow(<Login/>);
+        wrapper.find('input[name="email"]').simulate('change',{
+            target: {
+                value:'abc@email.com'
+            }
+        })
+
+        const email = wrapper.find('input[name="email"]').prop('value');
+        expect(email).toBe('abc@email.com'); 
+    })
+
+    test("on enter password state should be updated",() => {
+        
+        wrapper.find('input[name="password"]').simulate('change',{
+            target:{
+                value:'abc@123'
+            }
+        });
+
+        const password = wrapper.find('input[name="password"]').prop('value');
+        expect(password).toBe('abc@123');
+    })
+
+
 })
